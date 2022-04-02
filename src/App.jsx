a1f7e7d2d2ea52339da22ffffc25f1e234ebfc8d
twitzz <2351061127@qq.com.com>
2022-04-02 09:55:12 +0800

    feat: init

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,5 @@
 import React from "react";
-import useStore from "./store";
-import { observer } from "mobx-react-lite";
 
-function App() {
-  const rootStore = useStore();
-  return (
-    <div>
-      <h1>{rootStore.counterStore.count}</h1>
-      <button onClick={rootStore.counterStore.addCount}>+</button>
-      <h1>{rootStore.listStore.filterList.join("-")}</h1>
-      <button onClick={rootStore.listStore.addList}>修改list</button>
-    </div>
-  );
+export default function App() {
+  return <div>App</div>;
 }
-
-export default observer(App);
