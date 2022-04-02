69cdc7691ce8972074e86777818b0f16866b2157
twitzz <2351061127@qq.com.com>
2022-04-02 09:36:32 +0800

    feat: computed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import counterStore from "./store/counter";
+import listStore from "./store/list";
 import { observer } from "mobx-react-lite";
 
 function App() {
@@ -7,6 +8,8 @@ function App() {
     <div>
       <h1>{counterStore.count}</h1>
       <button onClick={counterStore.addCount}>+</button>
+      <h1>{listStore.filterList.join("-")}</h1>
+      <button onClick={listStore.addList}>修改list</button>
     </div>
   );
 }
---
+++ src/store/list.js
@@ -0,0 +1,20 @@
+import { makeAutoObservable } from "mobx";
+
+class ListStore {
+  list = [1, 2, 3, 4, 5];
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  get filterList() {
+    return this.list.filter((item) => item > 2);
+  }
+
+  addList = () => {
+    this.list.push(6, 7, 8);
+  };
+}
+
+const listStore = new ListStore();
+
+export default listStore;
